187d1e9816207d2e042727bffaf1bed722144042
Fabrizzio Rutigliano <fabrizzio.rutigliano@gdn.com.uy>
2026-02-22 00:16:45 -0300

    Revision de Expresion
    1. Cambio crear estructurado expresion a funcion.
    2. Corrección de formatos y agregar comentarios.
    3. Se crea testeo.

--- Expresion.cpp
+++ Expresion.cpp
@@ -1,17 +1,21 @@
 #include "Expresion.h"
 
-
-void expresionCrear(ArbolExpresion arbol, int indice, Expresion &exp)
+// Carga un Struct expresion con su índice y árbol de terminos.
+Expresion expresionCrear(ArbolExpresion arbol, int indice)
 {
+    Expresion exp;
     exp.indice=indice;
     exp.terminos=arbol;
+    return exp;
 }
 
+// Devuelve el indice de la expresión.
 int expresionDevolverIndice(Expresion exp)
 {
     return exp.indice;
 }
 
+// Devuelve el árbol expresión del estructurado expresión.
 ArbolExpresion expresionDevolverArbolExpresion(Expresion expresion)
 {
     return expresion.terminos;
