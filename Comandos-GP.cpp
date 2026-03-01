933105a4c2c40851c72187b33072825f0f6a6770
Guillermo <guillermopg25@gmail.com>
2026-03-01 15:15:50 -0300

    comando calcaular ajustado con controles

--- Comandos-GP.cpp
+++ Comandos-GP.cpp
@@ -1,22 +1,44 @@
 #include "Comandos.h"
 
 //Sintaxis del compando verificada antes de llamar a la funcion en MAIN
-void comandoCalcular(ListaString lstring, listaExp)
+void comandoCalcular(ListaString lstring, ListaExpresion listaExp)
 {
-    if (listaStringCantElementos(listaStr) != 2)
-    if (posLista >= 1)
+    if (listaStringCantElementos(lstring) != 3)
     {
-        Boolean error = FALSE;
-        int resultado; 
-        resultado = arbolExpresionEvaluar(expresionDevolverArbolExpresion(listaExpresionDevolverExpresionXIndice(listaExp, posLista)), valor, error);
-        if(error == TRUE)
+        String str,str2;
+        strCrear(str);
+        strCrear(str2);
+        listaStringObtener(2, lstring, str);
+        if(strEsEnteroPositivo(str))
         {
-            //mensaje error division 0
-            //tipoErrorDesplegar(ERROR_DIVISION_POR_CERO);
+            int posLista = strStringToInt(str);
+            listaStringObtener(3, lstring, str2);
+            if(strEsEntero(str2))
+            {
+                int valor = strStringToInt(str2);
+                Boolean error = FALSE;
+                int resultado; 
+                resultado = arbolExpresionEvaluar(expresionDevolverArbolExpresion(listaExpresionDevolverExpresionXIndice(listaExp, posLista)), valor, error);
+                if(error == TRUE)
+                {
+                    //mensaje error division 0
+                    //tipoErrorDesplegar(ERROR_DIVISION_POR_CERO);
+                }
+                else
+                    printf("\nEl resultado de calcular la expresion es... %d\n", resultado);
+            }
+            else
+                //mensaje de error no es entero 
         }
         else
-            printf("\nEl resultado de calcular la expresion es... %d\n", resultado);
+            //mensaje de error no es entero positivo
+        strDestruir(str);
+        strDestruir(str2);
+
     }
+    else    
+        //error cantidad de parametros
+    strDestruir(str);
 }
 
 
